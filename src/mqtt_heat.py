9c811d2712fb410518f5064d10118cafe4ea20ac
Jason Boileau <jason@boileau.dk>
2024-12-12 13:05:36 +0100

    Fix recent heating offset

--- src/mqtt_heat.py
+++ src/mqtt_heat.py
@@ -83,7 +83,7 @@ class MqttHeatControl():
     night_modifier_peak_hour = 16
     keep_warm_threshold = 20
     uv_modifier_factor = 50
-    recent_heat_offset_factor = 0.15
+    recent_heat_offset_factor = 0.25
 
     config_options_mqtt = ['pump_topic', 'update_freq', 'latitude', 'longitude', 'night_adjust_factor', 'keep_warm_modifier', 'keep_warm_ignore_cycles', 'keep_warm_history_hours', 'recent_heat_offset_history_hours', 'recent_heat_offset_factor', 'night_modifier_peak_hour', 'keep_warm_threshold', 'uv_modifier_factor']
     config_options = [*config_options_mqtt, 'topic_prefix', 'homeassistant_prefix', 'mqtt_server_ip', 'mqtt_server_port', 'mqtt_server_user', 'mqtt_server_password', 'rooms', 'unique_id_suffix', 'weather_today_topic', 'weather_tomorrow_topic']
@@ -311,7 +311,7 @@ class MqttHeatControl():
                 # Adjustment for heat already in the floor, but not disappated yet
                 heat_history = room['heat_history'][-offset_history_len:]
                 if len(heat_history):
-                    adj = sum(heat_history) * 3600 / self.update_freq * self.recent_heat_offset_factor
+                    adj = sum(heat_history) * self.update_freq * (1/3600) * self.recent_heat_offset_factor
                     modifier_pid -= adj
                     logger.debug(f'Applying adjustment for recent heating: ${adj:.0f}')
 
